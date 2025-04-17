ac2e8a39837370d850e4060c34b650923e435440
SpyD17711 <Spudeni@yandex.ru>
2025-04-17 15:48:18 +0300

    fix for deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173, // Использует $PORT от Render
+    strictPort: true, // Закрывает сервер, если порт занят
+  },
+  preview: {
+    port: process.env.PORT || 5173,
+  }
 })
-
